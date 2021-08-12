4aec79b369fc86d905d633b24e7b53366a227ca2
alsoerc <exkapp@gmail.com>
2021-08-11 21:03:34 -0500

    added employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,3 +1,6 @@
+import { Employee } from './../models/Employee';
+import { Url } from './../utils/url';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,36 @@ import { Injectable } from '@angular/core';
 })
 export class EmployeeService {
 
-  constructor() { }
+private url : string;
+
+  constructor(private service : HttpClient) {
+    this.url = Url.url;
+  }
+
+  getRecords(){
+    return this.service.get<Employee[]>(this.url + 'users');
+  }
+
+  postRecord(employee : Employee){
+    return this.service.post<Employee>(this.url + 'users', employee);
+  }
+
+  updateRecord(employee : Employee, id:number){
+    return this.service.put(this.url + 'users/' + id, employee);
+  }
+
+  deleteRecord(id:number){
+    return this.service.delete(this.url + 'users/' + id);
+  }
+
+  validateCredentials(employee : Employee){
+    return this.service.post<Employee>(this.url + 'users/login', employee);
+  }
+
+  recoverPassword(employee:Employee){
+    return this.service.put(this.url + 'users/recoverPassword', employee );
+  }
+
+
+
 }
